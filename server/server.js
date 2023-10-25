5f6683189a8312c41bacf9b23724cc995f63a563
Brian Rono <chikoe26@gmail.com>
2023-10-25 17:54:16 +0300

    frontend setup

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ const db = process.env.mongoURL;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(cors({
+  origin: 'https://bursary-management-system-frontend.vercel.app'
+}));
 
 // Use passport middleware and define passport strategy
 app.use(passport.initialize());
